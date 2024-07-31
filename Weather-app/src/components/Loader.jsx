04eee66960a69e578461d457266f9bb5417e146a
esraaali138 <esraaali1999138@gmail.com>
2024-07-31 16:32:32 -0700

    add test and made the app responsive

--- Weather-app/src/components/Loader.jsx
+++ Weather-app/src/components/Loader.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import { PuffLoader } from "react-spinners";
 
 const Loader = () => {
   return (
-    <div>
-   Loader   
+    <div className="flex justify-center mt-10">
+      <PuffLoader color="#bac2ce" size={80} />
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
---
+++ Weather-app/src/components/SearchBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SearchBar = ({setCity}) => {
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  return (
+    <div className="md:w-[500px] w-[400px] flex justify-between mx-auto mt-4">
+      <input
+        className="p-2 w-full text-[#bac2ce] outline-0 rounded-xl bg-primary placeholder-[#bac2ce]"
+        type="search"
+        placeholder="Search for cities"
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
